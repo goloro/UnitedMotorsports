8c96af9a697c59d4f5341264876d1d1be908011b
goloro <goloro02@gmail.com>
2021-12-04 21:42:14 +0100

    v2.1 responsive

--- buscador.js
+++ buscador.js
@@ -10,3 +10,14 @@ document.querySelector(".search").addEventListener("submit", e => {
         window.open("/Buscador/buscar.html", "_self")
     }
 })
+
+document.querySelector(".searchRes").addEventListener("submit", e => {
+    e.preventDefault();
+
+    let txt = buscador.value
+
+    if (txt != "" && txt != " ") {
+        localStorage.setItem("buscador", txt)
+        window.open("/Buscador/buscar.html", "_self")
+    }
+})
